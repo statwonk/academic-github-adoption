e96810f9285cbb47ecd543892e8c02794e2d1a5b
Christopher Peters <cpeter9@gmail.com>
2013-12-15 20:08:14 -0500

    Tidied up a bit

--- analysis.R
+++ analysis.R
@@ -1,9 +1,12 @@
-# A look at latex usage on github
+# Author: Christopher Peters
+# Email: cpeter9@gmail.com
+# A look at the number of public github repositories coded in LaTex
+# that are created each month.
 library(ggplot2)
 library(lubridate)
 library(ggthemes)
 
-df <- read.csv("~/installations/python/github/data.csv", header = F)
+df <- read.csv("data.csv", header = F)
 names(df) <- c("repo", "created_at")
 df$created_at <- as.POSIXct(df$created_at)
 df <- df[df$created_at < max(df$created_at) - days(14), ]
@@ -18,10 +21,8 @@ ggplot(df, aes(x = created_at)) +
   geom_histogram(colour = "black", fill = "tan", binwidth = 60*60*24*30.5, stat = "bin") +
   xlab("") +
   ylab("") +
-  ggtitle("Public LaTeX repositories created on Github\nper Month") +
+  ggtitle("Public Github Repositories\nwritten in LaTeX per Month") +
   theme_solarized(light = F) + 
   theme(axis.text.x = element_text(size = 32),
         axis.text.y = element_text(size = 28),
         plot.title = element_text(size = 30))
-
-
